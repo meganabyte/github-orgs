8a569b6b4123b21f7f610e8d5153f743f5fade09
meganabyte <51874806+meganabyte@users.noreply.github.com>
2019-08-18 21:36:57 -0700

    Update issues.go

--- issues/issues.go
+++ issues/issues.go
@@ -2,24 +2,25 @@ package issues
 
 import (
 	"context"
+	"sort"
 	"github.com/google/go-github/github"
+	"github.com/chenjiandongx/go-echarts/charts"
 )
 
-func GetRepoIssues(ctx context.Context, client *github.Client, orgName string, repos []*github.Repository) ([]*github.Issue,
-	[]*github.IssueComment, []*github.IssueEvent, error) {
+func GetRepoIssues(ctx context.Context, client *github.Client, orgName string,
+				   repos []*github.Repository, username string ) ([]*github.Issue, error) {
 	var list []*github.Issue
-	var comments []*github.IssueComment
-	var events []*github.IssueEvent
 	for _, repo := range repos {
 		repoName := repo.GetName()
 		repoOwner := repo.GetOwner().GetLogin()
 		opt := &github.IssueListByRepoOptions{
-			State:       "all",
+			Creator: username,
+			State: "all",
 			ListOptions: github.ListOptions{PerPage: 30}}
 		for {
 			l, resp, err := client.Issues.ListByRepo(ctx, repoOwner, repoName, opt)
 			if err != nil {
-				return nil, nil, nil, err
+				return nil, err
 			}
 			list = append(list, l...)
 			if resp.NextPage == 0 {
@@ -28,25 +29,39 @@ func GetRepoIssues(ctx context.Context, client *github.Client, orgName string, r
 			opt.Page = resp.NextPage
 		}
 	}
-	return list, comments, events, nil
+	return list, nil
 }
 
 func GetIssuesCreated(ctx context.Context, orgName string, client *github.Client, username string, 
-					  repos []*github.Repository) (map[string]int, error) {
-	list, _, _, err := GetRepoIssues(ctx, client, orgName, repos)
+					  repos []*github.Repository, m map[string]int) (error) {
+	list, err := GetRepoIssues(ctx, client, orgName, repos, username)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	m := make(map[string]int)
 	for _, issue := range list {
-		if issue.GetUser().GetLogin() == username {
-			time := issue.GetCreatedAt().Format("2006-01-02")
-			if val, ok := m[time]; !ok {
-				m[time] = 1
-			} else {
-				m[time] = val + 1
-			}
+		time := issue.GetCreatedAt().Format("2006-01-02")
+		if val, ok := m[time]; !ok {
+			m[time] = 1
+		} else {
+			m[time] = val + 1
 		}
 	}
-	return m, nil
+	return nil
+}
+
+func IssuesBase(m map[string]int) *charts.Bar {
+	var keys []string
+	nameItems := []string{}
+	countItems := []int{}
+	for k, v := range m {
+		keys = append(keys, k)
+		countItems = append(countItems, v)
+	}
+	sort.Strings(keys)
+	for _, k := range keys {
+		nameItems = append(nameItems, k)
+	}
+	bar := charts.NewBar()
+	bar.AddXAxis(nameItems).AddYAxis("Issues Opened", countItems)
+	return bar
 }
