a6cb49536ea473a9fe39b530771a578053dfa5f4
meganabyte <51874806+meganabyte@users.noreply.github.com>
2019-09-08 18:43:40 -0700

    Update repos.go

--- repos/repos.go
+++ repos/repos.go
@@ -2,8 +2,13 @@ package repos
 
 import (
 	"context"
-
 	"github.com/google/go-github/github"
+	"github.com/meganabyte/github-orgs/commits"
+	"github.com/meganabyte/github-orgs/issues"
+	"github.com/meganabyte/github-orgs/pulls"
+	"log"
+	"time"
+
 )
 
 func GetRepos(ctx context.Context, orgName string, client *github.Client) ([]*github.Repository, error) {
@@ -21,4 +26,29 @@ func GetRepos(ctx context.Context, orgName string, client *github.Client) ([]*gi
 		opt.Page = resp.NextPage
 	}
 	return list, nil
+}
+
+func FetchContributions(repos []*github.Repository, ctx context.Context, orgName string, client *github.Client, username string,
+						i map[string]int, c map[string]int, p map[string]int, yearAgo time.Time) {
+	for _, repo := range repos {
+		if repo.GetSize() != 0 {
+			repoName := repo.GetName()
+			repoOwner := repo.GetOwner().GetLogin()
+			err := issues.GetIssuesCreated(ctx, orgName, client, username, i, yearAgo, repoName, repoOwner)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			err = commits.GetUserCommits(ctx, orgName, client, username, c, yearAgo, repoName, repoOwner)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			err = pulls.GetUserPulls(ctx, orgName, client, username, p, yearAgo, repoName, repoOwner)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+		}
+	}
 }
